0002a04c2c1e1744aa049966aaab42dfe30f0a6f
conor.ney <conor.ux@gmail.com>
2022-10-09 00:37:44 -0400

    added error handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/duexcoast/greetings"
 )
 
 func main() {
-	// get a greeting message and print it
-	message := greetings.Hello("Gladyss")
+	// set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, sourcefile, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greetings message
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned message
+	// to the console.
+
 	fmt.Println(message)
 }
